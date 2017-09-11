a328d7e7db1b917eef093cbde82addb9eb8cde31
jinhyeok20171702 <jinhyeok17@kookmin.ac.kr>
2017-09-12 08:46:49 +0900

    Update 20171702-정진혁-assignment2.py

--- first_homework/20171702-정진혁-assignment2.py
+++ first_homework/20171702-정진혁-assignment2.py
@@ -1,10 +1,11 @@
-le(n !=-1):
+n = 1
+while(n != -1):
     num = 1
     fac_num = int(input("숫자 입력: "))
     for a in range(1,fac_num+1):
-        num = a*num
+        num *= a
 
-    if (fac_num == -1):
+    if (fac_num <= -1):
         break
 
     print(num)
